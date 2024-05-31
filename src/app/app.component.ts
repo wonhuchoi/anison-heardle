09bcc57b660c7375c42f3ee8e329db4395eed247
Wonhu Choi <wonhuchoi@gmail.com>
2024-05-30 22:25:25 -0700

    inital

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import * as data from "../assets/songs.json";
+const seedrandom = require('seedrandom');
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,81 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'anime-heardle';
+  guess = '';
+  songs;
+  songNumber;
+  songObj;
+  audio;
+  currentGuess = 0;
+  timeTable = {
+    0: 3,
+    1: 5,
+    2: 10,
+    3: 15,
+    4: 20,
+    5: 30
+  }
+  loser = false;
+  finished = false;
+  // day = (DateTime.Today - new DateTime(2000,1,1)).TotalDays;
+  
+  ngOnInit() {
+    let generator = seedrandom((new Date()).toUTCString);
+    console.log(generator());
+    console.log((new Date()).toUTCString);
+    // let 
+    this.songs = data.default;
+    console.log(this.songs)
+    this.songNumber = Math.floor(generator() * (this.songs.length));
+    this.songObj = this.songs[this.songNumber]
+    console.log(this.songObj)
+    
+    // $("#audio")[0].src = this.singObj.src;
+    setTimeout(() => {
+      // let audioObj = document.createElement("AUDIO");
+      // audioObj.src = this.songObj.src;
+      // audioObj.id = "audio";
+      $("#audio")[0].src = this.songObj.src;
+      // console.log("jsdlkfjlsdkfjkldsf", audioObj);
+      // $("#audioContainer")[0].appendChild(audioObj);
+    }, 500)
+  }
+
+  getAudio() {
+
+  }
+
+  checkTime(event) {
+    console.log("checkingTime", $("#audio")[0].currentTime);
+    if($("#audio")[0].currentTime > this.timeTable[this.currentGuess]) {
+      $("#audio")[0].currentTime = 0;
+      $("#audio")[0].pause();
+    }
+  }
+
+  doGuess() {
+    console.log("guessing", this.guess)
+    if(this.guess == "") {
+      return;
+    }
+    this.currentGuess++;
+    let qs = "#guess" + this.currentGuess;
+    $(qs)[0].innerHTML = this.guess;
+    if(this.guess == this.songObj.title || this.guess == this.songObj.anime[0]) {
+      $(qs)[0].style
+      this.finished = true;
+    } else {
+      if(this.currentGuess >= 6) {
+        this.finished = true;
+        this.loser = true;
+      }
+      $(qs)[0].style.background = "red"
+    }
+    this.resetGuess();
+  }
+
+  resetGuess() {
+    this.guess = "";
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Component} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {FormsModule} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
